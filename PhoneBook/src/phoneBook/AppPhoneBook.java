9ff5708e0859feba21880997c5cdffd463fec4d3
regan035 <regan035@gmail.comgit config --global user.name regan035git statusgit config --global user.email regan035@gmail.com>
2020-06-01 15:47:57 -0500

    update

--- PhoneBook/src/phoneBook/AppPhoneBook.java
+++ PhoneBook/src/phoneBook/AppPhoneBook.java
@@ -140,7 +140,7 @@ public class AppPhoneBook {
 		if(searchName.getFirstName().equals(first) && searchName.getMiddleName().equals(middle) && searchName.getLastName().equals(last)) {
 			System.out.println(entries[i].toString());
 		}
-		else{System.out.println("No record ");
+		else{System.out.println("No record found ");
 		}	
 		}
 	}
